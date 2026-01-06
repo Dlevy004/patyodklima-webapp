4b202937d5baf493f42e0d1d83665c8b4c4a89a5
Daróczi Levente Tibor <d.l3v1.deik@mailbox.unideb.hu>
2026-01-06 17:19:03 +0100

    chore: import css files in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import './css/cookies.css'
+import './css/global.css'
+import './css/styles.css'
+import './css/responsive.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
