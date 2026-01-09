e55be73c4141c2b7536366eaeb1cb7014ac305e1
Daróczi Levente Tibor <d.l3v1.deik@mailbox.unideb.hu>
2026-01-09 16:09:34 +0100

    feat: create 'Reference' component and register in 'App.jsx'

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Services from './components/Services'
+import Reference from './components/Reference'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <main>
         <Hero />
         <Services />
+        <Reference />
       </main>
     </>
   )
