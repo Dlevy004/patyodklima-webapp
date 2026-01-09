0d3eeddeea12ed8203237e6362a949391a22cbca
Daróczi Levente Tibor <d.l3v1.deik@mailbox.unideb.hu>
2026-01-09 16:14:08 +0100

    feat: create 'Contact.jsx' component and register in 'App.jsx'

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Services from './components/Services'
 import Reference from './components/Reference'
+import Contact from './components/Contact'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Hero />
         <Services />
         <Reference />
+        <Contact />
       </main>
     </>
   )
---
+++ frontend/src/components/Contact.jsx
@@ -0,0 +1,45 @@
+import './Contact.css'
+import ContactItem from './ContactItem.jsx'
+
+function Contact() {
+    return (
+        <section id="contact">
+            {/*Photo by Irvin Zheng on Unsplash*/}
+            <div className="content">
+                <h2 className="sub-title">Elérhetőségeink</h2>
+                <div className="contacts">
+                    <ContactItem 
+                        defaultIconSrc = '../assets/icons/call_24dp_F_FILL0_wght400_GRAD0_opsz24.svg'
+                        hoverIconSrc = '../assets/icons/call_24dp_228FCE_FILL1_wght400_GRAD0_opsz24.svg'
+                        contactText = '06 30 629 0793'
+                        contactHref = 'tel:+36306290793'
+                    />
+
+                    <ContactItem 
+                        defaultIconSrc = '../assets/icons/thumb_up_24dp_F_FILL0_wght400_GRAD0_opsz24.svg'
+                        hoverIconSrc = '../assets/icons/thumb_up_24dp_FFFFFF_FILL1_wght400_GRAD0_opsz24.svg'
+                        contactText = 'Kövess Facebookon!'
+                        contactHref = 'https://www.facebook.com/profile.php?id=61577473041862'
+                    />
+
+                    <ContactItem 
+                        defaultIconSrc = '../assets/icons/mail_24dp_F_FILL0_wght400_GRAD0_opsz24.svg'
+                        hoverIconSrc = '../assets/icons/mail_blue.svg'
+                        contactText = 'Írj nekünk e-mailt!'
+                        contactHref = 'mailto:klima.patyod@gmail.com'
+                    />
+                </div>
+            </div>
+
+            <div className="footer-wave">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" aria-hidden="true">
+                    <path fill-opacity="1"
+                        d="M0,160L80,170.7C160,181,320,203,480,202.7C640,203,800,181,960,170.7C1120,160,1280,160,1360,160L1440,160L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z">
+                    </path>
+                </svg>
+            </div>
+        </section>
+    )
+}
+
+export default Contact;
