eb066a29a6431c6ee83c9dcef777734871907ce5
Levente <daroczilevente2004@gmail.com>
2026-06-15 10:32:48 +0200

    refactor(App): restructure main component and add routing for Home and Privacy Policy pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,15 @@
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import Services from './components/Services'
-import Reference from './components/Reference'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
-import ScrollUp from './components/ScrollUp'
-import CookiePanel from './components/CookiePanel'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import PrivacyPolicy from './pages/PrivacyPolicy'
 
 function App() {
   return (
-    <>
-      <Navbar />
-
-      <main>
-        <Hero />
-        <Services />
-        <Reference />
-        <Contact />
-        <Footer />
-        <ScrollUp />
-        <CookiePanel />
-      </main>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} ></Route>
+        <Route path='/privacypolicy' element={<PrivacyPolicy />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
