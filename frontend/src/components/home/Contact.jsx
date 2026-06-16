cb9b7509594cc977c6e4290df84b9eefc3df6f58
Levente <daroczilevente2004@gmail.com>
2026-06-16 13:04:27 +0200

    refactor: remove duplicated icon SVGs and update Contact components to use icon components

--- frontend/src/components/home/Contact.jsx
+++ frontend/src/components/home/Contact.jsx
@@ -1,13 +1,10 @@
 import './Contact.css'
 import ContactItem from './ContactItem.jsx'
-import Wave from '@/components/common/Wave'
 
-import defaultCallIcon from '@/assets/icons/call_24dp_F_FILL0_wght400_GRAD0_opsz24.svg'
-import hoverCallIcon from '@/assets/icons/call_24dp_228FCE_FILL1_wght400_GRAD0_opsz24.svg'
-import defaultLikeIcon from '@/assets/icons/thumb_up_24dp_F_FILL0_wght400_GRAD0_opsz24.svg'
-import hoverLikeIcon from '@/assets/icons/thumb_up_24dp_FFFFFF_FILL1_wght400_GRAD0_opsz24.svg'
-import defaultMailIcon from '@/assets/icons/mail_24dp_F_FILL0_wght400_GRAD0_opsz24.svg'
-import hoverMailIcon from '@/assets/icons/mail_blue.svg'
+import Wave from '@/components/common/Wave'
+import PhoneIcon from '@/components/icons/PhoneIcon'
+import FacebookIcon from '@/components/icons/FacebookIcon'
+import MailIcon from '@/components/icons/MailIcon'
 
 function Contact() {
     return (
@@ -17,22 +14,22 @@ function Contact() {
                 <h2 className="sub-title">Elérhetőségeink</h2>
                 <div className="contacts">
                     <ContactItem 
-                        defaultIconSrc = {defaultCallIcon}
-                        hoverIconSrc = {hoverCallIcon}
+                        IconComponent = {PhoneIcon}
+                        isColorChange = {true}
                         contactText = '06 30 629 0793'
                         contactHref = 'tel:+36306290793'
                     />
 
                     <ContactItem 
-                        defaultIconSrc = {defaultLikeIcon}
-                        hoverIconSrc = {hoverLikeIcon}
+                        IconComponent = {FacebookIcon}
+                        isColorChange = {true}
                         contactText = 'Kövess Facebookon!'
                         contactHref = 'https://www.facebook.com/profile.php?id=61577473041862'
                     />
 
                     <ContactItem 
-                        defaultIconSrc = {defaultMailIcon}
-                        hoverIconSrc = {hoverMailIcon}
+                        IconComponent = {MailIcon}
+                        isColorChange = {true}
                         contactText = 'Írj nekünk e-mailt!'
                         contactHref = 'mailto:klima.patyod@gmail.com'
                     />
--- frontend/src/components/home/ContactItem.jsx
+++ frontend/src/components/home/ContactItem.jsx
@@ -1,22 +1,17 @@
 import PropTypes from 'prop-types'
 
-function ContactItem ({ defaultIconSrc, hoverIconSrc, contactText, contactHref }) {
+function ContactItem ({ IconComponent, isColorChange = false, contactText, contactHref }) {
     return (
         <div className="contact">
-            <img 
-                className="black-icon" 
-                src={defaultIconSrc} alt="" aria-hidden="true"/>
-            <img 
-                className="blue-icon" 
-                src={hoverIconSrc} alt="" aria-hidden="true"/>
+            <IconComponent className="contact-icon" isColorChange = {isColorChange}/>
             <a href={contactHref} target="_blank" rel="noopener noreferrer">{contactText}</a>
         </div>
     )
 }
 
 ContactItem.propTypes = {
-    defaultIconSrc: PropTypes.string.isRequired,
-    hoverIconSrc: PropTypes.string.isRequired,
+    IconComponent: PropTypes.elementType.isRequired,
+    isColorChange: PropTypes.bool,
     contactText: PropTypes.string.isRequired,
     contactHref: PropTypes.string.isRequired
 }
