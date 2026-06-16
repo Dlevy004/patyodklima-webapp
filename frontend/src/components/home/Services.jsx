c8f31e02fdae22f3b81dd30f89f6618b104b2e4d
Levente <daroczilevente2004@gmail.com>
2026-06-16 12:33:52 +0200

    refactor: replace assignment icon with AssignmentIcon component

--- frontend/src/components/home/Services.jsx
+++ frontend/src/components/home/Services.jsx
@@ -1,11 +1,11 @@
 import ServiceCard from './ServiceCard.jsx'
 import './Services.css'
 import assignmentImg from '@/assets/images/felmeres.avif'
-import assignmentIcon from '@/assets/icons/assignment.svg'
+import AssignmentIcon from '@/components/icons/AssignmentIcon'
 import installationImg from '@/assets/images/telepites.avif'
-import installationIcon from '@/assets/icons/handyman.svg'
+import HandymanIcon from '@/components/icons/HandymanIcon'
 import maintenanceImg from '@/assets/images/karbantartas.avif'
-import maintenanceIcon from '@/assets/icons/cleaning.svg'
+import CleaningIcon from '@/components/icons/CleaningIcon'
 
 function Services() {
     return (
@@ -15,21 +15,21 @@ function Services() {
                 <ServiceCard 
                     imgSrc = {assignmentImg}
                     title = "Díjmentes felmérés"
-                    bgIcon = {assignmentIcon}
+                    IconComponent = {AssignmentIcon}
                     desc = "Az előzetes egyeztetést követően személyesen mérjük fel a helyszínt, hogy pontos ajánlatot adhassunk."
                     altText = "Díjmentes felmérés"
                 />
                 <ServiceCard
                     imgSrc = {installationImg}
                     title = "Teljeskörű telepítés"
-                    bgIcon = {installationIcon}
+                    IconComponent = {HandymanIcon}
                     desc = "Vállaljuk klímaberendezések szakszerű telepítését. A készüléket precízen, az Ön igényeihez igazodva helyezzük üzembe, hogy hosszú távon megbízhatóan működjön."
                     altText = "Teljeskörű telepítés"
                 />
                 <ServiceCard
                     imgSrc = {maintenanceImg}
                     title = "Karbantartás"
-                    bgIcon = {maintenanceIcon}
+                    IconComponent = {CleaningIcon}
                     desc = "Szakemberünk gondoskodik klímája rendszeres karbantartásáról és tisztításáról &mdash; így nemcsak hatékonyabban működik, hanem a levegő is tisztább marad."
                     altText = "Karbantartás"
                 />
