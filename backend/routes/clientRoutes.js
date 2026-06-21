47e1dbeada988724e1ce975f3e5268a2e02b86b8
Daróczi Levente <daroczilevente2004@gmail.com>
2026-06-21 14:31:01 +0200

    feat: add endpoint to routes script

--- backend/routes/clientRoutes.js
+++ backend/routes/clientRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const clientController = require('../controllers/clientController');
 
+router.post('/', clientController.createClient);
 router.get('/clients', clientController.getAllClients);
 router.get('/client/:id', clientController.getClientById);
 
