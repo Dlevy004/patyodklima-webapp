736270a0d4b6f4c633bfb7ee4c6f153ec5722316
Daróczi Levente <daroczilevente2004@gmail.com>
2026-06-21 14:29:11 +0200

    feat: implement createClient

--- backend/services/clientService.js
+++ backend/services/clientService.js
@@ -1,5 +1,20 @@
 const prisma = require('../database/prisma');
 
+const createClient = async (newClient) => {
+    return await prisma.clients.create({
+        data: {
+            full_name: newClient.full_name,
+            phone: newClient.phone,
+            email: newClient.email,
+            zip_code: newClient.zip_code,
+            city: newClient.city,
+            street_address: newClient.street_address,
+            type: newClient.type || "individual",
+            notes: newClient.notes
+        }
+    });
+}
+
 const getAllClients = async () => {
     return await prisma.clients.findMany();
 }
@@ -13,6 +28,7 @@ const getClientById = async (id) => {
 }
 
 module.exports = {
+    createClient,
     getAllClients,
     getClientById
 };
