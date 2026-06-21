73895f796c7d14aadd97cd153004119f84e5142f
Daróczi Levente <daroczilevente2004@gmail.com>
2026-06-21 14:30:31 +0200

    feat: handle messages for post method

--- backend/controllers/clientController.js
+++ backend/controllers/clientController.js
@@ -1,13 +1,25 @@
 const clientService = require('../services/clientService');
 
+const createClient = async (req, res) => {
+    try {
+        const newClient = req.body;
+        const createdClient = await clientService.createClient(newClient);
+        res.status(201).json(createdClient);
+    }
+    catch (error) {
+        console.error('Error while creating client:', error.message);
+        res.status(400).json({ message: 'An error occurred while creating client.' })
+    }
+}
+
 const getAllClients = async (req, res) => {
     try {
         const clients = await clientService.getAllClients();
         res.status(200).json(clients);
     }
     catch (error) {
-        console.error('Error while getting all clients:', error);
-        res.status(500).json({ error: 'An error occurred while getting clients.' });
+        console.error('Error while getting all clients:', error.message);
+        res.status(500).json({ message: 'An error occurred while getting clients.' });
     }
 }
 
@@ -18,12 +30,13 @@ const getClientById = async (req, res) => {
         res.status(200).json(client);
     }
     catch (error) {
-        console.error('Error while getting client by ID:', error);
-        res.status(500).json({ error: 'An error occurred while getting client.' })
+        console.error('Error while getting client by ID:', error.message);
+        res.status(500).json({ message: 'An error occurred while getting client.' })
     }
 }
 
 module.exports = {
+    createClient,
     getAllClients,
     getClientById
 };
