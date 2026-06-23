8cf128033c03084a3f07d73f6c1ccfad44d2e474
Daróczi Levente <daroczilevente2004@gmail.com>
2026-06-23 12:59:40 +0200

    feat: add route for dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import PrivacyPolicy from './pages/PrivacyPolicy'
+import Dashboard from './pages/admin/Dashboard'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} ></Route>
-        <Route path='/privacypolicy' element={<PrivacyPolicy />}></Route>
+        <Route path='/' element={<Home />} />
+        <Route path='/privacypolicy' element={<PrivacyPolicy />} />
+        <Route path='/admin' element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   )
