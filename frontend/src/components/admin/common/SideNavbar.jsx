b8befda384c442d34139112342c2b52145f9f7f3
Daróczi Levente <daroczilevente2004@gmail.com>
2026-06-24 21:30:19 +0200

    refactor: outsource adminNavLinks for the navbar menu's buttons

--- frontend/src/components/admin/common/SideNavbar.jsx
+++ frontend/src/components/admin/common/SideNavbar.jsx
@@ -26,6 +26,24 @@ function SideNavbar() {
         localStorage.setItem('isSidebarCollapsed', isCollapsed);
     }, [isCollapsed]);
 
+    const adminNavLinks = (
+        <>
+            <NavButton IconComponent={HomeIcon} title='Főoldal' url='/admin' />
+            <NavSection title='Adminisztráció' ButtonComponents={[
+                <NavButton key='client-page' IconComponent={ClientPageIcon} title='Ügyfélnapló' url='/admin/clients' />,
+                <NavButton key='job-page' IconComponent={JobPageIcon} title='Munkanapló' url='/admin/jobs' />,
+                <NavButton key='error-page' IconComponent={ErrorIcon} title='Bejelentések' url='/admin/errors' />,
+            ]} />
+            <NavSection title='Értékesítés' ButtonComponents={[
+                <NavButton key='ai-page' IconComponent={VisualDesignIcon} title='Látványterv' url='/admin/visualdesigns' />
+            ]} />
+            <NavSection title='Marketing' ButtonComponents={[
+                <NavButton key='reference-page' IconComponent={PhotoGalleryIcon} title='Referencia' url='/admin/references' />,
+                <NavButton key='ads-page' IconComponent={HvacIcon} title='Hirdetések' url='/admin/ads' />
+            ]} />
+        </>
+    )
+
     return (
         <>
             <div className='admin-mobile-header'>
@@ -37,19 +55,7 @@ function SideNavbar() {
                     <a href='/admin'>
                         <img src={isCollapsed ? verticalLogo : horizontalLogo} className='pkLogo' alt='Pátyod Klíma logo'/>
                     </a>
-                    <NavButton IconComponent={HomeIcon} title='Főoldal' className='sidebar-btn' url='/admin' />
-                    <NavSection title='Adminisztráció' ButtonComponents={[
-                        <NavButton key='client-page' IconComponent={ClientPageIcon} title='Ügyfélnapló' url='/admin/clients' />,
-                        <NavButton key='job-page' IconComponent={JobPageIcon} title='Munkanapló' url='/admin/jobs' />,
-                        <NavButton key='error-page' IconComponent={ErrorIcon} title='Bejelentések' url='/admin/errors' />,
-                    ]} />
-                    <NavSection title='Értékesítés' ButtonComponents={[
-                        <NavButton key='ai-page' IconComponent={VisualDesignIcon} title='Látványterv' url='/admin/visualdesigns' />
-                    ]} />
-                    <NavSection title='Marketing' ButtonComponents={[
-                        <NavButton key='reference-page' IconComponent={PhotoGalleryIcon} title='Referencia' url='/admin/references' />,
-                        <NavButton key='ads-page' IconComponent={HvacIcon} title='Hirdetések' url='/admin/ads' />
-                    ]} />
+                    {adminNavLinks}
                 </div>
                 <button className='hide-btn'
                     onClick={() => setIsCollapsed(prev => !prev)}>
@@ -58,20 +64,8 @@ function SideNavbar() {
             </div>
 
             <MobileMenu isOpen={isMobileMenuOpen} onClose={() => setIsMobileMenuOpen(false)} className='admin-mobile-menu' >
-                <div className='admin-mobile-links'>
-                    <NavButton IconComponent={HomeIcon} title='Főoldal' url='/admin' />
-                    <NavSection title='Adminisztráció' ButtonComponents={[
-                        <NavButton key='client-page' IconComponent={ClientPageIcon} title='Ügyfélnapló' url='/admin/clients' />,
-                        <NavButton key='job-page' IconComponent={JobPageIcon} title='Munkanapló' url='/admin/jobs' />,
-                        <NavButton key='error-page' IconComponent={ErrorIcon} title='Bejelentések' url='/admin/errors' />,
-                    ]} />
-                    <NavSection title='Értékesítés' ButtonComponents={[
-                        <NavButton key='ai-page' IconComponent={VisualDesignIcon} title='Látványterv' url='/admin/visualdesigns' />
-                    ]} />
-                    <NavSection title='Marketing' ButtonComponents={[
-                        <NavButton key='reference-page' IconComponent={PhotoGalleryIcon} title='Referencia' url='/admin/references' />,
-                        <NavButton key='ads-page' IconComponent={HvacIcon} title='Hirdetések' url='/admin/ads' />
-                    ]} />
+                <div className='admin-mobile-links' >
+                    {adminNavLinks}
                 </div>
             </MobileMenu>
         </>
