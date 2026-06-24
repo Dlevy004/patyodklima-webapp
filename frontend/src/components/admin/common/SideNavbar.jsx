0eaa648ee33f707c8e55d2424d41221f76d672f3
Daróczi Levente <daroczilevente2004@gmail.com>
2026-06-24 20:56:11 +0200

    feat: added hamburger menu for side navbar component for mobile view

--- frontend/src/components/admin/common/SideNavbar.jsx
+++ frontend/src/components/admin/common/SideNavbar.jsx
@@ -1,7 +1,5 @@
 import { useState, useEffect } from 'react'
-
 import './SideNavbar.css'
-
 import NavButton from './NavButton'
 import NavSection from './NavSection'
 import HomeIcon from '../../icons/HomeIcon'
@@ -14,8 +12,11 @@ import HvacIcon from '../../icons/HvacIcon'
 import HideIcon from '../../icons/BackToTopIcon'
 import horizontalLogo from '../../../assets/images/logo.avif'
 import verticalLogo from '../../../assets/images/tr-logo-icon.avif'
+import HamburgerMenu from '../../common/HamburgerMenu'
+import MobileMenu from '../../common/MobileMenu'
 
 function SideNavbar() {
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
     const [isCollapsed, setIsCollapsed] = useState(() => {
         const savedState = localStorage.getItem('isSidebarCollapsed');
         return savedState === 'true';
@@ -26,30 +27,54 @@ function SideNavbar() {
     }, [isCollapsed]);
 
     return (
-        <div className={isCollapsed ? 'sidenav-collapsed' : 'sidenav-container' }>
-            <div className='sidenav-top'>
-                <a href='/admin'>
-                    <img src={isCollapsed ? verticalLogo : horizontalLogo} className='pkLogo' alt='Pátyod Klíma logo'/>
-                </a>
-                <NavButton IconComponent={HomeIcon} title='Főoldal' className='sidebar-btn' url='/admin' />
-                <NavSection title='Adminisztráció' ButtonComponents={[
-                    <NavButton key='client-page' IconComponent={ClientPageIcon} title='Ügyfélnapló' url='/admin/clients' />,
-                    <NavButton key='job-page' IconComponent={JobPageIcon} title='Munkanapló' url='/admin/jobs' />,
-                    <NavButton key='error-page' IconComponent={ErrorIcon} title='Bejelentések' url='/admin/errors' />,
-                ]} />
-                <NavSection title='Értékesítés' ButtonComponents={[
-                    <NavButton key='ai-page' IconComponent={VisualDesignIcon} title='Látványterv' url='/admin/visualdesigns' />
-                ]} />
-                <NavSection title='Marketing' ButtonComponents={[
-                    <NavButton key='reference-page' IconComponent={PhotoGalleryIcon} title='Referencia' url='/admin/references' />,
-                    <NavButton key='ads-page' IconComponent={HvacIcon} title='Hirdetések' url='/admin/ads' />
-                ]} />
+        <>
+            <div className='admin-mobile-header'>
+                <HamburgerMenu onClick={() => setIsMobileMenuOpen(true)} />
             </div>
-            <button className='hide-btn'
-                onClick={() => setIsCollapsed(prev => !prev)}>
-                <HideIcon />
-            </button>
-        </div>
+
+            <div className={isCollapsed ? 'sidenav-collapsed' : 'sidenav-container' }>
+                <div className='sidenav-top'>
+                    <a href='/admin'>
+                        <img src={isCollapsed ? verticalLogo : horizontalLogo} className='pkLogo' alt='Pátyod Klíma logo'/>
+                    </a>
+                    <NavButton IconComponent={HomeIcon} title='Főoldal' className='sidebar-btn' url='/admin' />
+                    <NavSection title='Adminisztráció' ButtonComponents={[
+                        <NavButton key='client-page' IconComponent={ClientPageIcon} title='Ügyfélnapló' url='/admin/clients' />,
+                        <NavButton key='job-page' IconComponent={JobPageIcon} title='Munkanapló' url='/admin/jobs' />,
+                        <NavButton key='error-page' IconComponent={ErrorIcon} title='Bejelentések' url='/admin/errors' />,
+                    ]} />
+                    <NavSection title='Értékesítés' ButtonComponents={[
+                        <NavButton key='ai-page' IconComponent={VisualDesignIcon} title='Látványterv' url='/admin/visualdesigns' />
+                    ]} />
+                    <NavSection title='Marketing' ButtonComponents={[
+                        <NavButton key='reference-page' IconComponent={PhotoGalleryIcon} title='Referencia' url='/admin/references' />,
+                        <NavButton key='ads-page' IconComponent={HvacIcon} title='Hirdetések' url='/admin/ads' />
+                    ]} />
+                </div>
+                <button className='hide-btn'
+                    onClick={() => setIsCollapsed(prev => !prev)}>
+                    <HideIcon />
+                </button>
+            </div>
+
+            <MobileMenu isOpen={isMobileMenuOpen} onClose={() => setIsMobileMenuOpen(false)} className='admin-mobile-menu' >
+                <div className='admin-mobile-links'>
+                    <NavButton IconComponent={HomeIcon} title='Főoldal' url='/admin' />
+                    <NavSection title='Adminisztráció' ButtonComponents={[
+                        <NavButton key='client-page' IconComponent={ClientPageIcon} title='Ügyfélnapló' url='/admin/clients' />,
+                        <NavButton key='job-page' IconComponent={JobPageIcon} title='Munkanapló' url='/admin/jobs' />,
+                        <NavButton key='error-page' IconComponent={ErrorIcon} title='Bejelentések' url='/admin/errors' />,
+                    ]} />
+                    <NavSection title='Értékesítés' ButtonComponents={[
+                        <NavButton key='ai-page' IconComponent={VisualDesignIcon} title='Látványterv' url='/admin/visualdesigns' />
+                    ]} />
+                    <NavSection title='Marketing' ButtonComponents={[
+                        <NavButton key='reference-page' IconComponent={PhotoGalleryIcon} title='Referencia' url='/admin/references' />,
+                        <NavButton key='ads-page' IconComponent={HvacIcon} title='Hirdetések' url='/admin/ads' />
+                    ]} />
+                </div>
+            </MobileMenu>
+        </>
     )
 }
 
