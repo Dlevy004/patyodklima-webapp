796adb3bf9468e01c27a9a87bf6b61d364aea22c
Daróczi Levente <daroczilevente2004@gmail.com>
2026-06-26 18:39:44 +0200

    feat: add useDocumentTitle and implement it in Home and PrivacyPolicy pages

--- frontend/src/pages/PrivacyPolicy.jsx
+++ frontend/src/pages/PrivacyPolicy.jsx
@@ -3,8 +3,11 @@ import Footer from '@/components/common/Footer'
 import ScrollUp from '@/components/common/ScrollUp'
 import CookiePanel from '@/components/common/CookiePanel'
 import PrivacyContent from '@/components/privacypolicy/PrivacyContent'
+import useDocumentTitle from './useDocumentTitle'
 
 function PrivacyPolicy() {
+    useDocumentTitle('Pátyod Klíma | Adatkezelési tájékoztató')
+
     return(
         <>
             <Navbar />
