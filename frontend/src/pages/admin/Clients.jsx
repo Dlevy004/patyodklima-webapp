b7810a975655d99d42d2f9db87d7d2b8291411ec
Daróczi Levente <daroczilevente2004@gmail.com>
2026-06-28 14:37:12 +0200

    feat: implement client fetching from API and display loading/error messages

--- frontend/src/pages/admin/Clients.jsx
+++ frontend/src/pages/admin/Clients.jsx
@@ -3,21 +3,54 @@ import usePageTitle from '../../hooks/usePageTitle';
 import TableHeader from '../../components/admin/common/TableHeader';
 import './Clients.css'
 import ClientItem from '../../components/admin/clients/ClientItem';
+import { useState, useEffect } from 'react';
 
 function Clients() {
     usePageTitle('Ügyfélnapló');
+
+    const [clients, setClients] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchClients = async () => {
+            try {
+                const response = await fetch('http://localhost:3000/api/clients');
+
+                if (!response.ok) {
+                    throw new Error('Failed to retrieve data from the server.');
+                }
+
+                const data = await response.json();
+                setClients(data);
+            } catch (error) {
+                setError(error.message)
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        fetchClients();
+    }, []);
 
     return (
         <>
             <TableHeader />
             <div className='table-content'>
-                <ClientItem name='Kis PistaPistaPistaPista' city='Település nevePistaPistaPista' phone='06 20 1234 567'/>
-                <ClientItem name='Kis Pista' city='Település neve' phone='06 20 1234 567'/>
-                <ClientItem name='Kis Pista' city='Település neve' phone='06 20 1234 567'/>
-                <ClientItem name='Kis Pista' city='Település neve' phone='06 20 1234 567'/>
-                <ClientItem name='Kis Pista' city='Település neve' phone='06 20 1234 567'/>
-                <ClientItem name='Kis Pista' city='Település neve' phone='06 20 1234 567'/>
-                <ClientItem name='Kis Pista' city='Település neve' phone='06 20 1234 567'/>
+                {isLoading && <p className='info-text'>Ügyfelek betöltése folyamatban...</p>}
+                {error && <p className='info-text error-text'>Hiba: {error}</p>}
+
+                {!isLoading && !error && clients.length === 0
+                && <p className='info-text'>Még nincsenek rögzített ügyfelek az adatbázisban.</p>}
+
+                {!isLoading && !error && clients.map((client) => (
+                    <ClientItem
+                        key={client.id}
+                        name={client.full_name}
+                        city={client.city}
+                        phone={client.phone}
+                    />
+                ))}
             </div>
             <ScrollUp />
         </>
