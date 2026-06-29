bc27da8cf81281015e88b82e5c2d36fbfc17cef9
Daróczi Levente <daroczilevente2004@gmail.com>
2026-06-29 12:38:35 +0200

    fix: change initial state of data in useFetch hook from array to null

--- frontend/src/hooks/useFetch.js
+++ frontend/src/hooks/useFetch.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 
 function useFetch(url) {
-    const [data, setData] = useState([]);
+    const [data, setData] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
 
