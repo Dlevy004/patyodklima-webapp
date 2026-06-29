aeb6013df47b7892dc087f620823406eb1a17c43
Daróczi Levente <daroczilevente2004@gmail.com>
2026-06-29 16:25:21 +0200

    feat: add DataStateFeedback component for improved loading and error handling in Clients page

--- frontend/src/pages/admin/Clients.jsx
+++ frontend/src/pages/admin/Clients.jsx
@@ -1,32 +1,36 @@
 import ScrollUp from '../../components/common/ScrollUp'
-import usePageTitle from '../../hooks/usePageTitle';
-import TableHeader from '../../components/admin/common/TableHeader';
+import usePageTitle from '../../hooks/usePageTitle'
+import TableHeader from '../../components/admin/common/TableHeader'
 import './Clients.css'
-import ClientItem from '../../components/admin/clients/ClientItem';
-import useFetch from '../../hooks/useFetch';
+import ClientItem from '../../components/admin/clients/ClientItem'
+import useFetch from '../../hooks/useFetch'
+import DataStateFeedback from '../../components/admin/common/DataStateFeedback'
 
 function Clients() {
     usePageTitle('Ügyfélnapló');
-    const { data: clients, isLoading, error } = useFetch('http://localhost:3000/api/clients');
+    const { data: clients = [], isLoading, error } = useFetch('http://localhost:3000/api/clients');
+
+    const isEmpty = !isLoading && !error && clients?.length === 0;
 
     return (
         <>
             <TableHeader />
             <div className='table-content'>
-                {isLoading && <p className='info-text'>Ügyfelek betöltése folyamatban...</p>}
-                {error && <p className='info-text error-text'>Hiba: {error}</p>}
-
-                {!isLoading && !error && clients.length === 0
-                && <p className='info-text'>Még nincsenek rögzített ügyfelek az adatbázisban.</p>}
-
-                {!isLoading && !error && clients.map((client) => (
-                    <ClientItem
-                        key={client.id}
-                        name={client.full_name}
-                        city={client.city}
-                        phone={client.phone}
-                    />
-                ))}
+                <DataStateFeedback
+                    isLoading={isLoading}
+                    error={error}
+                    isEmpty={isEmpty}
+                    emptyMessage={'Még nincsenek rögzített ügyfelek az adatbázisban.'}
+                >
+                    {clients?.map((client) => (
+                        <ClientItem
+                            key={client.id}
+                            name={client.full_name}
+                            city={client.city}
+                            phone={client.phone}
+                        />
+                    ))}
+                </DataStateFeedback>
             </div>
             <ScrollUp />
         </>
