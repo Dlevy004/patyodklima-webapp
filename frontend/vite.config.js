d84d5e9ac4b62dd0dfdba51aa855ed52a3d27d13
Daróczi Levente <daroczilevente2004@gmail.com>
2026-06-29 16:28:07 +0200

    test: installed vitest and react testing library dependencies

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/setupTests.js'
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
