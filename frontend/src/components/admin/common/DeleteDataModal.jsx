25caaee6c2bbd7ec390a4680b168a14d6fad03d6
Daróczi Levente <daroczilevente2004@gmail.com>
2026-07-02 14:40:47 +0200

    refactor: rename CSS classes for consistency

--- frontend/src/components/admin/common/DeleteDataModal.jsx
+++ frontend/src/components/admin/common/DeleteDataModal.jsx
@@ -4,13 +4,13 @@ import PropTypes from 'prop-types';
 function DeleteDataModal({ titleData, descriptionData, onClose, onDelete }) {
     return (
         <div className='delete-modal'>
-            <div className='delete-title-bg'>
+            <div className='title-bg'>
                 <h1>{titleData} törlése</h1>
             </div>
             <p className='description'>
                 Biztos vagy benne, hogy törlöd a(z) {descriptionData}?
             </p>
-            <div className='delete-buttons'>
+            <div className='modal-buttons'>
                 <button className='modal-close-btn' onClick={onClose}>Mégse</button>
                 <button className='modal-delete-btn' onClick={onDelete}>Törlés</button>
             </div>
