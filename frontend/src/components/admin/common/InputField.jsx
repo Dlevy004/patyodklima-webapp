74c899dc91a09c91854043fdcf612dd8b807181e
Daróczi Levente <daroczilevente2004@gmail.com>
2026-07-02 16:17:01 +0200

    feat: add InputField component with styling and prop types

---
+++ frontend/src/components/admin/common/InputField.jsx
@@ -0,0 +1,36 @@
+import './InputField.css'
+import PropTypes from 'prop-types';
+
+function InputField({ label, type, value, onChange, placeholder, pattern }) {
+    return (
+        <div className='input-field'>
+            {label && <label>{label}</label>}
+            {type === 'textarea' ? (
+                <textarea
+                    value={value}
+                    onChange={onChange}
+                    placeholder={placeholder}
+                />
+            ) : (
+                <input
+                    type={type}
+                    value={value}
+                    onChange={onChange}
+                    placeholder={placeholder}
+                    pattern={pattern}
+                />
+            )}
+        </div>
+    )
+}
+
+InputField.propTypes = {
+    label: PropTypes.string,
+    type: PropTypes.string.isRequired,
+    value: PropTypes.string,
+    onChange: PropTypes.func.isRequired,
+    placeholder: PropTypes.string,
+    pattern: PropTypes.string
+}
+
+export default InputField;
