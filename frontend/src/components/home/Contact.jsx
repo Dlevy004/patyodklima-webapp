09c9fbdbc4d73d247d964f16335e929976a3cee3
Daróczi Levente <daroczilevente2004@gmail.com>
2026-07-09 20:17:07 +0200

    refactor: replace multiple components with an array logic

--- frontend/src/components/home/Contact.jsx
+++ frontend/src/components/home/Contact.jsx
@@ -10,32 +10,44 @@ import MailIcon from '@/components/icons/MailIcon'
 
 
 function Contact() {
+    const contactItems = [
+        {
+            IconComponent: PhoneIcon,
+            isColorChange: true,
+            contactText: '06 30 629 0793',
+            contactHref: 'tel:+36306290793'
+        },
+        {
+            IconComponent: FacebookIcon,
+            isColorChange: true,
+            contactText: 'Kövess Facebookon!',
+            contactHref: 'https://www.facebook.com/profile.php?id=61577473041862'
+        },
+        {
+            IconComponent: MailIcon,
+            isColorChange: true,
+            contactText: 'Írj nekünk e-mailt!',
+            contactHref: 'mailto:klima.patyod@gmail.com'
+        }
+    ];
+
     return (
         <section id="contact">
             {/*Photo by Irvin Zheng on Unsplash*/}
             <div className="content">
                 <h2 className="sub-title">Elérhetőségeink</h2>
                 <div className="contacts">
-                    <ContactItem 
-                        IconComponent = {PhoneIcon}
-                        isColorChange = {true}
-                        contactText = '06 30 629 0793'
-                        contactHref = 'tel:+36306290793'
-                    />
-
-                    <ContactItem 
-                        IconComponent = {FacebookIcon}
-                        isColorChange = {true}
-                        contactText = 'Kövess Facebookon!'
-                        contactHref = 'https://www.facebook.com/profile.php?id=61577473041862'
-                    />
-
-                    <ContactItem 
-                        IconComponent = {MailIcon}
-                        isColorChange = {true}
-                        contactText = 'Írj nekünk e-mailt!'
-                        contactHref = 'mailto:klima.patyod@gmail.com'
-                    />
+                    {
+                        contactItems.map((item, key) => (
+                            <ContactItem
+                                key = {key}
+                                IconComponent = {item.IconComponent}
+                                isColorChange = {item.isColorChange}
+                                contactText = {item.contactText}
+                                contactHref = {item.contactHref}
+                            />
+                        ))
+                    }
                 </div>
                 <Wave />
             </div>
--- frontend/src/components/home/Services.jsx
+++ frontend/src/components/home/Services.jsx
@@ -12,31 +12,46 @@ import CleaningIcon from '@/components/icons/CleaningIcon'
 
 
 function Services() {
+    const serviceCards = [
+        {
+            imgSrc: assignmentImg,
+            title: "Díjmentes felmérés",
+            IconComponent: AssignmentIcon,
+            desc: "Az előzetes egyeztetést követően személyesen mérjük fel a helyszínt, hogy pontos ajánlatot adhassunk.",
+            altText: "Díjmentes felmérés"
+        },
+        {
+            imgSrc: installationImg,
+            title: "Teljeskörű telepítés",
+            IconComponent: HandymanIcon,
+            desc: "Vállaljuk klímaberendezések szakszerű telepítését. A készüléket precízen, az Ön igényeihez igazodva helyezzük üzembe, hogy hosszú távon megbízhatóan működjön.",
+            altText: "Teljeskörű telepítés"
+        },
+        {
+            imgSrc: maintenanceImg,
+            title: "Karbantartás",
+            IconComponent: CleaningIcon,
+            desc: "Szakemberünk gondoskodik klímája rendszeres karbantartásáról és tisztításáról &mdash; így nemcsak hatékonyabban működik, hanem a levegő is tisztább marad.",
+            altText: "Karbantartás"
+        }
+    ];
+
     return (
         <section id="services">
             <h2 className="sub-title">Szolgáltatásaink</h2>
             <div className="cards">
-                <ServiceCard 
-                    imgSrc = {assignmentImg}
-                    title = "Díjmentes felmérés"
-                    IconComponent = {AssignmentIcon}
-                    desc = "Az előzetes egyeztetést követően személyesen mérjük fel a helyszínt, hogy pontos ajánlatot adhassunk."
-                    altText = "Díjmentes felmérés"
-                />
-                <ServiceCard
-                    imgSrc = {installationImg}
-                    title = "Teljeskörű telepítés"
-                    IconComponent = {HandymanIcon}
-                    desc = "Vállaljuk klímaberendezések szakszerű telepítését. A készüléket precízen, az Ön igényeihez igazodva helyezzük üzembe, hogy hosszú távon megbízhatóan működjön."
-                    altText = "Teljeskörű telepítés"
-                />
-                <ServiceCard
-                    imgSrc = {maintenanceImg}
-                    title = "Karbantartás"
-                    IconComponent = {CleaningIcon}
-                    desc = "Szakemberünk gondoskodik klímája rendszeres karbantartásáról és tisztításáról &mdash; így nemcsak hatékonyabban működik, hanem a levegő is tisztább marad."
-                    altText = "Karbantartás"
-                />
+                {
+                    serviceCards.map((card, key) => (
+                        <ServiceCard
+                            key = {key}
+                            imgSrc = {card.imgSrc}
+                            title = {card.title}
+                            IconComponent = {card.IconComponent}
+                            desc = {card.desc}
+                            altText = {card.altText}
+                        />
+                    ))
+                }
             </div>
         </section>
     )
