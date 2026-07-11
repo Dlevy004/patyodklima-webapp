8968bc6e85ac4d5ec19003dac513cb177302aed0
Daróczi Levente <daroczilevente2004@gmail.com>
2026-07-11 19:50:22 +0200

    test: add unit tests in useDeleteData test

--- frontend/src/hooks/useDeleteData.test.js
+++ frontend/src/hooks/useDeleteData.test.js
@@ -1,6 +1,8 @@
 import { renderHook, act } from '@testing-library/react';
 import { describe, it, expect, vi, beforeEach } from 'vitest';
 
+import toast from 'react-hot-toast';
+
 import useDeleteData from './useDeleteData';
 
 
@@ -10,6 +12,8 @@ describe('useDeleteData', () => {
     beforeEach(() => {
         vi.clearAllMocks();
     });
+
+    vi.mock('react-hot-toast');
 
     it('should return true on successful deletion', async () => {
         fetch.mockResolvedValue({ ok: true });
@@ -26,6 +30,20 @@ describe('useDeleteData', () => {
         expect(result.current.error).toBeNull();
     });
 
+    it('should render hot toast message on successful deletion', async () => {
+        fetch.mockResolvedValue({ ok: true });
+
+        const { result } = renderHook(() => useDeleteData());
+
+        let success;
+        await act(async () => {
+            success = await result.current.deleteData('/api/clients/1');
+        });
+
+        expect(success).toBe(true);
+        expect(toast.success).toHaveBeenCalledWith('Sikeresen törölve!');
+    });
+
     it('should return false and set error on failure', async () => {
         fetch.mockResolvedValue({ ok: false });
 
@@ -39,5 +57,19 @@ describe('useDeleteData', () => {
         expect(success).toBe(false);
         expect(result.current.error).toBe('Failed to delete data from the server.');
         expect(result.current.isDeleting).toBe(false);
+    });
+
+    it('should render hot toast message on failure', async () => {
+        fetch.mockResolvedValue({ ok: false });
+
+        const { result } = renderHook(() => useDeleteData());
+
+        let success;
+        await act(async () => {
+            success = await result.current.deleteData('/api/clients/1');
+        });
+
+        expect(success).toBe(false);
+        expect(toast.error).toHaveBeenCalledWith('Hiba történt a törlés során!');
     });
 });
