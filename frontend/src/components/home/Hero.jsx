a0a1638c9dc2109bc0e9c97da421e6f1ad20da3e
Daróczi Levente <daroczilevente2004@gmail.com>
2026-07-20 20:37:11 +0200

    refactor: update the hero's layout

--- frontend/src/components/home/Hero.jsx
+++ frontend/src/components/home/Hero.jsx
@@ -6,23 +6,20 @@ import heroImg from '@/assets/images/heroImg.avif'
 function Hero() {
     return (
         <section id="hero">
-            <h1 className="hero-title">Téli melegség, nyári frissesség!</h1>
+            <div className="hero-bg-container">
+                <img className="hero-image" src={heroImg} alt="Klíma szerelés és karbantartás" />
+                <div className="hero-overlay"></div>
+            </div>
             <div className="hero-main">
-                <div className="hero-left">
-                    <div className="hero-text">
-                        <p className="hero-description">
-                            Teljeskörű klímaszolgáltatás Pátyodon és 30 km-es körzetében.{' '}
-                            <span>Telepítés</span>, <span>karbantartás</span>, <span>tisztítás</span> &mdash;
-                            <mark>rövid határidővel</mark>, <mark>megbízhatóan</mark>, <mark>garanciával</mark>.
-                        </p>
-                        <p className="hero-description">
-                            Többféle típusú készüléket kínálunk különböző igényekhez és árkategóriákhoz.
-                        </p>
-                        <a className="hero-btn" href="#contact">Foglalj időpontot most!</a>
-                    </div>
-                </div>
-                <div className="hero-right">
-                    <img src={heroImg} alt="Beltéri egység telepítése, karbantartása"/>
+                <h1 className="hero-title">Téli melegség, nyári frissesség!</h1>
+                <div className="hero-text">
+                    <p className="hero-description">
+                        Teljeskörű klímaszolgáltatás Pátyodon és 30 km-es körzetében.{' '}
+                        <span>Telepítés</span>, <span>karbantartás</span>, <span>tisztítás</span> &mdash;
+                        rövid határidővel, megbízhatóan, garanciával. <br/> Többféle típusú készüléket kínálunk különböző 
+                        igényekhez és árkategóriákhoz.
+                    </p>
+                    <a className="hero-btn" href="#contact">Foglalj időpontot most!</a>
                 </div>
             </div>
         </section>
