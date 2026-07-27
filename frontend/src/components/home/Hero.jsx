099b3641dd17dfb71d1dca97e418b9a72540eae0
Daróczi Levente <daroczilevente2004@gmail.com>
2026-07-27 15:49:45 +0200

    feat: update hero layout and styles

--- frontend/src/components/home/Hero.jsx
+++ frontend/src/components/home/Hero.jsx
@@ -2,8 +2,25 @@ import './Hero.css'
 
 import heroImg from '@/assets/images/heroImg.avif'
 
+import Achievement from './Achievement';
+
 
 function Hero() {
+    const achievements = [
+        {
+            number: '5.0',
+            title: 'Google értékelés'
+        },
+        {
+            number: '50+',
+            title: 'Elégedett ügyfél'
+        },
+        {
+            number: '36',
+            title: 'Hónap garancia'
+        }
+    ];
+
     return (
         <section id="hero">
             <div className="hero-bg-container">
@@ -11,15 +28,28 @@ function Hero() {
                 <div className="hero-overlay"></div>
             </div>
             <div className="hero-main">
-                <h1 className="hero-title">Téli melegség, nyári frissesség!</h1>
-                <div className="hero-text">
-                    <p className="hero-description">
-                        Teljeskörű klímaszolgáltatás Pátyodon és 30 km-es körzetében.{' '}
-                        <span>Telepítés</span>, <span>karbantartás</span>, <span>tisztítás</span> &mdash;
-                        rövid határidővel, megbízhatóan, garanciával. <br/> Többféle típusú készüléket kínálunk különböző 
-                        igényekhez és árkategóriákhoz.
-                    </p>
-                    <a className="hero-btn" href="#contact">Foglalj időpontot most!</a>
+                <div className='hero-up'>
+                    <h1 className="hero-title">Téli melegség, nyári frissesség!</h1>
+                    <div className="hero-text">
+                        <p className="hero-description">
+                            Teljeskörű klímaszolgáltatás Pátyodon és 30 km-es körzetében.{' '}
+                            <span>Telepítés</span>, <span>karbantartás</span>, <span>tisztítás</span> &mdash;
+                            rövid határidővel, megbízhatóan, garanciával. <br/> Többféle típusú készüléket kínálunk különböző 
+                            igényekhez és árkategóriákhoz.
+                        </p>
+                        <a className="hero-btn" href="#contact">Foglalj időpontot most!</a>
+                    </div>
+                </div>
+                <div className='hero-achievements'>
+                    {
+                        achievements.map((item, key) => (
+                            <Achievement
+                                key={key}
+                                number={item.number}
+                                title={item.title}
+                            />
+                        ))
+                    }
                 </div>
             </div>
         </section>
