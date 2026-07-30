067fe6767e9a0a3ec2e44159edf99a85884dae5a
Daróczi Levente <daroczilevente2004@gmail.com>
2026-07-30 23:26:57 +0200

    fix: remove unnecessary lines between imports

--- frontend/src/pages/PrivacyPolicy.jsx
+++ frontend/src/pages/PrivacyPolicy.jsx
@@ -2,9 +2,7 @@ import Navbar from '@/components/common/Navbar'
 import Footer from '@/components/common/Footer'
 import ScrollUp from '@/components/common/ScrollUp'
 import CookiePanel from '@/components/common/CookiePanel'
-
 import PrivacyContent from '@/components/privacypolicy/PrivacyContent'
-
 import useDocumentTitle from '../hooks/useDocumentTitle'
 
 
