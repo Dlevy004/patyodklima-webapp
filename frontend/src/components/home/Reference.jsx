b1474dbe6118fd37680b1d688df51ebbf8abe555
Daróczi Levente <daroczilevente2004@gmail.com>
2026-08-01 20:24:50 +0200

    fix: added aria-labelledby to reference section

--- frontend/src/components/home/Reference.jsx
+++ frontend/src/components/home/Reference.jsx
@@ -5,7 +5,6 @@ import { Navigation, Pagination } from 'swiper/modules';
 import './Reference.css'
 
 import ReferenceCard from "./ReferenceCard";
-
 import ref4 from '@/assets/images/reference4.avif';
 import ref1 from '@/assets/images/reference1.avif';
 import ref11 from '@/assets/images/reference1-1.avif';
@@ -15,64 +14,64 @@ import ande from '@/assets/images/585059918_122150208692915768_33815419919614438
 import ande2 from '@/assets/images/585495273_122150208740915768_5983887693530440261_n.avif';
 import ce from '@/assets/images/585175757_122150209760915768_8231168429410044420_n.avif';
 
+const referencesData = [
+    {
+        id : 1,
+        imgSrc : ref4,
+        altText : 'Ande klíma készülék telepítése',
+        cardTitle : 'ANDE Klíma telepítése Pátyod'
+    },
+    {
+        id : 2,
+        imgSrc : ref1,
+        altText : 'Pátyod, vegyesbolt klimatizálása',
+        cardTitle : 'Vegyesbolt klimatizálása, AUX beltéri készülék'
+    },
+    {
+        id : 3,
+        imgSrc : ref11,
+        altText : 'Pátyod, vegyesbolt klimatizálása',
+        cardTitle : 'Vegyesbolt klimatizálása, AUX kültéri készülék'
+    },
+    {
+        id : 4,
+        imgSrc : ref2,
+        altText : 'Tisztítás',
+        cardTitle : 'Tisztítás'
+    },
+    {
+        id : 5,
+        imgSrc : aux,
+        altText : 'Hálószoba hűtése és fűtése AUX készülékkel',
+        cardTitle : 'Hálószoba hűtése és fűtése AUX készülékkel'
+    },
+    {
+        id : 6,
+        imgSrc : ande,
+        altText : 'ANDE beltéri készülék',
+        cardTitle : 'Családi ház klimatizálása, ANDE készülékkel'
+    },
+    {
+        id : 7,
+        imgSrc : ande2,
+        altText : 'ANDE kültéri készülék',
+        cardTitle : 'Családi ház klimatizálása, ANDE készülékkel'
+    },
+    {
+        id : 8,
+        imgSrc : ce,
+        altText : 'CEKLIMA kültéri készülék',
+        cardTitle : 'Hálószoba hűtése és fűtése CEKLIMA készülékkel'
+    }
+]
+
 
 function Reference() {
-    const referencesData = [
-        {
-            id : 1,
-            imgSrc : ref4,
-            altText : 'Ande klíma készülék telepítése',
-            cardTitle : 'ANDE Klíma telepítése Pátyod'
-        },
-        {
-            id : 2,
-            imgSrc : ref1,
-            altText : 'Pátyod, vegyesbolt klimatizálása',
-            cardTitle : 'Vegyesbolt klimatizálása, AUX beltéri készülék'
-        },
-        {
-            id : 3,
-            imgSrc : ref11,
-            altText : 'Pátyod, vegyesbolt klimatizálása',
-            cardTitle : 'Vegyesbolt klimatizálása, AUX kültéri készülék'
-        },
-        {
-            id : 4,
-            imgSrc : ref2,
-            altText : 'Tisztítás',
-            cardTitle : 'Tisztítás'
-        },
-        {
-            id : 5,
-            imgSrc : aux,
-            altText : 'Hálószoba hűtése és fűtése AUX készülékkel',
-            cardTitle : 'Hálószoba hűtése és fűtése AUX készülékkel'
-        },
-        {
-            id : 6,
-            imgSrc : ande,
-            altText : 'ANDE beltéri készülék',
-            cardTitle : 'Családi ház klimatizálása, ANDE készülékkel'
-        },
-        {
-            id : 7,
-            imgSrc : ande2,
-            altText : 'ANDE kültéri készülék',
-            cardTitle : 'Családi ház klimatizálása, ANDE készülékkel'
-        },
-        {
-            id : 8,
-            imgSrc : ce,
-            altText : 'CEKLIMA kültéri készülék',
-            cardTitle : 'Hálószoba hűtése és fűtése CEKLIMA készülékkel'
-        }
-    ]
-
     return (
-        <section id="reference">
-            <h2 className="sub-title">Referencia</h2>
+        <section id="reference" aria-labelledby='reference-section'>
+            <h2 id='reference-section' className="sub-title">Referencia</h2>
             <div className="reference-container">
-                <div className="ref-bg"></div>
+                <div className="ref-bg" aria-hidden='true'></div>
                 <div className="container">
                     <Swiper
                         modules={[Navigation, Pagination]}
