5d891a15cc3ecad5ffbc49715dc93fba0684e8a4
Daróczi Levente <daroczilevente2004@gmail.com>
2026-08-11 16:11:54 +0200

    chore: added HelmetProvider to main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HelmetProvider } from 'react-helmet-async'
 
 import App from './App.jsx'
 
@@ -7,7 +8,9 @@ import './css/global.css'
 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <HelmetProvider>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </HelmetProvider>
 )
