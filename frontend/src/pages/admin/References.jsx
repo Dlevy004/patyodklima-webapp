5f9fb8d1cc1840e22322131ed9bb222ace806eee
Daróczi Levente <daroczilevente2004@gmail.com>
2026-08-13 14:17:40 +0200

    feat: add drag-and-drop UI and styling for references page

--- frontend/src/pages/admin/References.jsx
+++ frontend/src/pages/admin/References.jsx
@@ -1,17 +1,31 @@
-import ScrollUp from '../../components/common/ScrollUp'
+import './References.css'
 
-import usePageTitle from '../../hooks/usePageTitle';
+import ScrollUp from '@/components/common/ScrollUp'
+import usePageTitle from '@/hooks/usePageTitle';
+import DragAndDrop from '@/components/admin/references/DragAndDrop';
+import InputField from '@/components/admin/common/InputField';
 
 
 function References() {
     usePageTitle('Referenciák');
 
     return (
-        <main style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '100vh' }}>
-            <h1>References</h1>
-            <p>Welcome to the references page!</p>
+        <>
+            <div className='references-container'>
+                <DragAndDrop onFileSelect={(file) => console.log('Selected file:', file)} />
+                <div className='image-description-container'>
+                    <InputField
+                        label='Leírás hozzáadása'
+                        type='text'
+                        required={true}
+                        onChange={(e) => handleInputChange('description', e)}
+                    />
+                    <button className='submit-btn' type='submit' aria-label='Feltöltés'>Feltöltés</button>
+                </div>
+            </div>
+
             <ScrollUp />
-        </main>
+        </>
     )
 }
 
