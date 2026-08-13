4a08531295265794f596c78b7ee26ae762c4fa94
Daróczi Levente <daroczilevente2004@gmail.com>
2026-08-13 17:01:07 +0200

    feat: enhance drag-and-drop component with file preview functionality

--- frontend/src/components/admin/references/DragAndDrop.jsx
+++ frontend/src/components/admin/references/DragAndDrop.jsx
@@ -3,18 +3,40 @@ import { Plus } from 'lucide-react'
 import './DragAndDrop.css'
 
 
-function DragAndDrop({ onFileSelect}) {
+function DragAndDrop({ onFileSelect, previewUrl }) {
+    const handleDragOver = (e) => {
+        e.preventDefault();
+    };
+
+    const handleDrop = (e) => {
+        e.preventDefault();
+        if (e.dataTransfer.files && e.dataTransfer.files.length > 0) {
+            onFileSelect(e.dataTransfer.files[0]);
+        }
+    };
+
     return (
-        <label className="drag-and-drop-container">
+        <label
+            className="drag-and-drop-container"
+            onDragOver={handleDragOver}
+            onDrop={handleDrop}
+        >
             <input
                 type="file"
+                accept="image/*"
                 onChange={(e) => onFileSelect(e.target.files[0])}
                 className="drag-and-drop-input"
             />
-            <Plus size={80} aria-hidden='true'/>
-            <span className="drag-and-drop-label">
-                Húzd ide a képet vagy kattints ide a feltöltéshez
-            </span>
+                {previewUrl ? (
+                    <img src={previewUrl} alt="Előnézet" className="drag-and-drop-preview" />
+                ) : (
+                    <>
+                        <Plus size={80} aria-hidden='true'/>
+                        <span className="drag-and-drop-label">
+                            Húzd ide a képet vagy kattints ide a feltöltéshez
+                        </span>
+                    </>
+                )}
         </label>
     )
 }
