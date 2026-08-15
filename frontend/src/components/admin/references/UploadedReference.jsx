f1dae213b4b7e099176e500d87838557bef67286
Daróczi Levente <daroczilevente2004@gmail.com>
2026-08-15 15:01:38 +0200

    feat: added grayscale filter when images are not visible

--- frontend/src/components/admin/references/UploadedReference.jsx
+++ frontend/src/components/admin/references/UploadedReference.jsx
@@ -5,18 +5,18 @@ import './UploadedReference.css';
 import ActionBtn from '../common/ActionBtn';
 
 
-function UploadedReference({ title, imageUrl, isVisible = true }) {
+function UploadedReference({ title, imageUrl, isVisible = true, onDelete, onEdit, onToggleVisibility }) {
     return (
-        <li className='uploaded-reference'>
+        <li className={`uploaded-reference ${isVisible ? '' : 'is-hidden'}`}>
             <img className='single-reference-img' src={imageUrl} alt={title} loading="lazy" />
             <div className='reference-actions'>
                 {
                     isVisible
-                        ? <ActionBtn type='visible' onClick={() => {}} />
-                        : <ActionBtn type='invisible' onClick={() => {}} />
+                        ? <ActionBtn type='visible' onClick={onToggleVisibility} />
+                        : <ActionBtn type='invisible' onClick={onToggleVisibility} />
                 }
-                <ActionBtn type='edit' onClick={() => {}} />
-                <ActionBtn type='delete' onClick={() => {}} />
+                <ActionBtn type='edit' onClick={onEdit} />
+                <ActionBtn type='delete' onClick={onDelete} />
             </div>
         </li>
     )
@@ -25,7 +25,10 @@ function UploadedReference({ title, imageUrl, isVisible = true }) {
 UploadedReference.propTypes = {
     title: PropTypes.string.isRequired,
     imageUrl: PropTypes.string.isRequired,
-    isVisible: PropTypes.bool
+    isVisible: PropTypes.bool,
+    onDelete: PropTypes.func.isRequired,
+    onEdit: PropTypes.func.isRequired,
+    onToggleVisibility: PropTypes.func.isRequired
 };
 
 export default UploadedReference;
