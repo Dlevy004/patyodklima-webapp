1ef40a7077066440aaeabd522fb958a8d0dc7a7b
Daróczi Levente <daroczilevente2004@gmail.com>
2026-08-26 21:21:08 +0200

    feat: implement job routes

---
+++ backend/routes/jobRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const jobController = require('../controllers/jobController');
+
+const router = express.Router();
+
+router.post('/jobs', jobController.createJob);
+router.get('/jobs', jobController.getAllJobs);
+router.get('/jobs/:id', jobController.getJobById);
+router.put('/jobs/:id', jobController.updateJob);
+router.delete('/jobs/:id', jobController.deleteJobs);
+
+module.exports = router;
