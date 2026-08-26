0cdaf8c85112541916c2405c269ebccde90a67a7
Daróczi Levente <daroczilevente2004@gmail.com>
2026-08-26 16:12:04 +0200

    refactor: move endpoint above

--- backend/index.js
+++ backend/index.js
@@ -9,22 +9,21 @@ const app = express();
 // Middlewares
 app.use(cors());
 app.use(express.json());
-
 app.set('trust proxy', 1);
 
 // Routes
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello World!' });
-});
-
-// Health
+// Health check
 app.get('/api/health', (req, res) => {
     res.status(200).send('OK');
 });
 
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello World!' });
+});
+
 app.use('/api', routes);
 
-//server start
+//Server start
 app.listen(process.env.PORT, () => {
   console.log(`The server started at http://localhost:${process.env.PORT}.`);
 });
