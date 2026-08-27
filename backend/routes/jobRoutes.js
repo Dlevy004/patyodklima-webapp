bbe4c68a2ffa42b035dab1f04d448a62a9ccfcb8
Daróczi Levente <daroczilevente2004@gmail.com>
2026-08-27 13:24:00 +0200

    fix: fixed typo

--- backend/routes/jobRoutes.js
+++ backend/routes/jobRoutes.js
@@ -7,6 +7,6 @@ router.post('/jobs', jobController.createJob);
 router.get('/jobs', jobController.getAllJobs);
 router.get('/jobs/:id', jobController.getJobById);
 router.put('/jobs/:id', jobController.updateJob);
-router.delete('/jobs/:id', jobController.deleteJobs);
+router.delete('/jobs/:id', jobController.deleteJob);
 
 module.exports = router;
