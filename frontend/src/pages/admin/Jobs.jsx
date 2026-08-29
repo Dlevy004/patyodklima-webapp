543f6e3d6ce35adf43b76e97c6252ab2606c13de
Daróczi Levente <daroczilevente2004@gmail.com>
2026-08-29 16:38:54 +0200

    feat: implement job page

--- frontend/src/pages/admin/Jobs.jsx
+++ frontend/src/pages/admin/Jobs.jsx
@@ -1,17 +1,103 @@
-import ScrollUp from '../../components/common/ScrollUp'
+import ScrollUp from '@/components/common/ScrollUp';
+import TableHeader from '@/components/admin/common/TableHeader';
+import DataStateFeedback from '@/components/admin/common/DataStateFeedback'
+import ModalBackdrop from '@/components/admin/common/ModalBackdrop'
+import JobItem from '@/components/admin/jobs/JobItem';
+import usePageTitle from '@/hooks/usePageTitle';
+import useModal from '@/hooks/useModal';
+import useDeleteData from '@/hooks/useDeleteData';
+import useSaveData from '@/hooks/useSaveData';
+import useFetch from '@/hooks/useFetch';
+import DeleteDataModal from '@/components/admin/common/DeleteDataModal';
+import AddEditJobModal from '@/components/admin/jobs/AddEditJobModal';
 
-import usePageTitle from '../../hooks/usePageTitle';
+const API_URL = `${import.meta.env.VITE_API_URL}/api/jobs`
+const CLIENT_API_URL = `${import.meta.env.VITE_API_URL}/api/clients`
 
 
 function Jobs() {
     usePageTitle('Munkanapló');
 
+    const { data: jobs = [], isLoading, error, refetch } = useFetch(API_URL);
+    const { data: clients = [] } = useFetch(CLIENT_API_URL);
+    const isEmpty = !isLoading && !error && jobs?.length === 0;
+
+    const deleteModal = useModal();
+    const editModal = useModal();
+
+    const { deleteData } = useDeleteData();
+    const { saveData } = useSaveData();
+
+    const handleConfirmDelete = async () => {
+        if (!deleteModal.selectedItem) return;
+
+        const success = await deleteData(`${API_URL}/${deleteModal.selectedItem.id}`);
+
+        if (success) {
+            deleteModal.close();
+            refetch();
+        }
+    };
+
+    const handleSaveClient = async (formData) => {
+        const isEditing = Boolean(editModal.selectedItem);
+        const url = isEditing ? `${API_URL}/${editModal.selectedItem.id}` : API_URL;
+        const method = isEditing ? 'PUT' : 'POST';
+
+        const success = await saveData(url, method, formData);
+
+        if (success) {
+            editModal.close();
+            refetch();
+        }
+    };
+
     return (
-        <main style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '100vh' }}>
-            <h1>Jobs</h1>
-            <p>Welcome to the jobs page!</p>
+        <>
+            <TableHeader onAddClick={() => editModal.open()} />
+
+            <div className='table-content'>
+                <DataStateFeedback
+                    isLoading={isLoading}
+                    error={error}
+                    isEmpty={isEmpty}
+                    emptyMessage={'Nincsenek rögzített munkák az adatbázisban.'}
+                >
+                    {jobs?.map((job) => (
+                        <JobItem
+                            key={job.id}
+                            client_name={job.client_id}
+                            category={job.category}
+                            date={job.date}
+                            isCompleted={false}
+                            onToggleStatus={false}
+                            onDelete={() => deleteModal.open(job)}
+                            onEdit={() => editModal.open(job)}
+                        />
+                    ))}
+                </DataStateFeedback>
+            </div>
+
+            <ModalBackdrop isOpen={deleteModal.isOpen} onClose={deleteModal.close}>
+                <DeleteDataModal
+                    titleData={'Munka'}
+                    descriptionData={'munkát'}
+                    onClose={deleteModal.close}
+                    onDelete={handleConfirmDelete}
+                />
+            </ModalBackdrop>
+
+            <ModalBackdrop isOpen={editModal.isOpen} onClose={editModal.close}>
+                <AddEditJobModal
+                    onClose={editModal.close}
+                    onSave={handleSaveClient}
+                    jobData={editModal.selectedItem}
+                    clientList={clients}
+                />
+            </ModalBackdrop>
+
             <ScrollUp />
-        </main>
+        </>
     )
 }
 
