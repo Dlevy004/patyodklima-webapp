fca4cd70cb21656812e61e1db7eedd197ce6dd80
Daróczi Levente <daroczilevente2004@gmail.com>
2026-08-29 20:19:37 +0200

    feat: add toast notifications for client deletion success and error handling

--- frontend/src/pages/admin/Clients.jsx
+++ frontend/src/pages/admin/Clients.jsx
@@ -1,3 +1,5 @@
+import toast from 'react-hot-toast'
+
 import './Clients.css'
 
 import ScrollUp from '@/components/common/ScrollUp'
@@ -35,6 +37,10 @@ function Clients() {
         if (success) {
             deleteModal.close();
             refetch();
+            toast.success('Ügyfél sikeresen törölve!');
+        } else {
+            toast.error('Ehhez az ügyfélhez még tartozik munka, így nem lehet törölni!');
+            deleteModal.close();
         }
     };
 
