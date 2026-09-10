79186fcbcf95e07d5e85bb0e13f525fdb34713e1
Daróczi Levente <daroczilevente2004@gmail.com>
2026-09-10 17:03:46 +0200

    fix: resolved toast error msg visibility

--- frontend/src/hooks/useReferenceUpload.js
+++ frontend/src/hooks/useReferenceUpload.js
@@ -87,7 +87,7 @@ export default function useReferenceUpload(onSuccess) {
             setDescription('');
 
         } catch (error) {
-            toast.error('Error during upload: ', error.message);
+            toast.error(`Error during upload: ${error.message}`);
         } finally {
             setIsUploading(false);
         }
