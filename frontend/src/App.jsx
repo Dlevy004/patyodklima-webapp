6403d06ca05baf6606cf0fda3f5556fba62f0cde
Daróczi Levente <daroczilevente2004@gmail.com>
2026-09-16 10:40:06 +0200

    chore: import useLayoutEffect and delete unused useEffect

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { useEffect } from 'react'
+import { useLayoutEffect } from 'react'
 
 import Home from './pages/Home'
 import PrivacyPolicy from './pages/PrivacyPolicy'
