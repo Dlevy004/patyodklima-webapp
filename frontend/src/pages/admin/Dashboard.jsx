de944c34c55d0ebdad7c54c32ef9f70ef97d51ef
Daróczi Levente <daroczilevente2004@gmail.com>
2026-09-20 19:08:25 +0200

    feat: import recent activity to dashboard page

--- frontend/src/pages/admin/Dashboard.jsx
+++ frontend/src/pages/admin/Dashboard.jsx
@@ -1,17 +1,18 @@
-import ScrollUp from '../../components/common/ScrollUp'
+import ScrollUp from '@/components/common/ScrollUp'
 
-import usePageTitle from '../../hooks/usePageTitle';
+import usePageTitle from '@/hooks/usePageTitle';
+import RecentActivity from '@/components/admin/dashboard/RecentActivity';
 
 
 function Dashboard() {
     usePageTitle('Főoldal');
 
     return (
-        <main style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '100vh' }}>
-            <h1>Dashboard</h1>
-            <p>Welcome to the admin dashboard!</p>
-            <ScrollUp />
-        </main>
+        <>
+            <RecentActivity/>
+
+            <ScrollUp/>
+        </>
     )
 }
 
