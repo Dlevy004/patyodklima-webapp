b8458c21212eddf5281b7ac7d4382b4b016f04f8
Daróczi Levente <daroczilevente2004@gmail.com>
2026-09-20 19:15:53 +0200

    feat: import stat card component to dashboard page

--- frontend/src/pages/admin/Dashboard.jsx
+++ frontend/src/pages/admin/Dashboard.jsx
@@ -2,6 +2,7 @@ import ScrollUp from '@/components/common/ScrollUp'
 
 import usePageTitle from '@/hooks/usePageTitle';
 import RecentActivity from '@/components/admin/dashboard/RecentActivity';
+import StatCard from '@/components/admin/dashboard/StatCard';
 
 
 function Dashboard() {
@@ -10,6 +11,7 @@ function Dashboard() {
     return (
         <>
             <RecentActivity/>
+            <StatCard/>
 
             <ScrollUp/>
         </>
