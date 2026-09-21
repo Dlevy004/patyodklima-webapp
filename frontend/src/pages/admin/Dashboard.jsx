1d2d3cede469ecc46e64329c05032c37bcf1db27
Daróczi Levente <daroczilevente2004@gmail.com>
2026-09-21 10:21:53 +0200

    feat: add styles to dashboard page

--- frontend/src/pages/admin/Dashboard.jsx
+++ frontend/src/pages/admin/Dashboard.jsx
@@ -1,5 +1,7 @@
 import ScrollUp from '@/components/common/ScrollUp'
 
+import './Dashboard.css'
+
 import usePageTitle from '@/hooks/usePageTitle';
 import RecentActivity from '@/components/admin/dashboard/RecentActivity';
 import StatCard from '@/components/admin/dashboard/StatCard';
