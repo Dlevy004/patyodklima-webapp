4e3b305922e9022cc9687efe6cf2c4340b330d72
Daróczi Levente <daroczilevente2004@gmail.com>
2026-09-21 10:22:21 +0200

    feat: create layout to dashboard page

--- frontend/src/pages/admin/Dashboard.jsx
+++ frontend/src/pages/admin/Dashboard.jsx
@@ -6,6 +6,10 @@ import usePageTitle from '@/hooks/usePageTitle';
 import RecentActivity from '@/components/admin/dashboard/RecentActivity';
 import StatCard from '@/components/admin/dashboard/StatCard';
 import MonthlyRevenueChart from '@/components/admin/dashboard/MonthlyRevenueChart';
+import RevenueByCategoryChart from '@/components/admin/dashboard/RevenueByCategoryChart';
+import TopAcUnitsChart from '@/components/admin/dashboard/TopAcUnitsChart';
+import TopCitiesChart from '@/components/admin/dashboard/TopCitiesChart';
+import JobsStatusChart from '@/components/admin/dashboard/JobsStatusChart';
 
 
 function Dashboard() {
@@ -14,8 +18,21 @@ function Dashboard() {
     return (
         <>
             <RecentActivity/>
-            <StatCard/>
-            <MonthlyRevenueChart/>
+
+            <div className='dashboard-wrapper'>
+                <StatCard/>
+                <MonthlyRevenueChart/>
+
+                <div className='dashboard-charts-row'>
+                    <RevenueByCategoryChart />
+                    <TopAcUnitsChart />
+                </div>
+
+                <div className='dashboard-charts-row'>
+                    <TopCitiesChart />
+                    <JobsStatusChart />
+                </div>
+            </div>
 
             <ScrollUp/>
         </>
