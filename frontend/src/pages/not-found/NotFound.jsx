58f5b8d98442f4ef90ada0a33c0fa1ebe01a3216
Daróczi Levente <daroczilevente2004@gmail.com>
2026-09-21 18:44:10 +0200

    feat: restructure NotFound component to include Seo for improved SEO

--- frontend/src/pages/not-found/NotFound.jsx
+++ frontend/src/pages/not-found/NotFound.jsx
@@ -3,27 +3,35 @@ import { Link } from 'react-router-dom';
 import './NotFound.css'
 
 import usePageTitle from '@/hooks/usePageTitle';
+import Seo from '@/components/common/Seo';
 
 
 export default function NotFound() {
     usePageTitle('Az oldal nem található');
-    /*seo component*/
 
     return (
-        <div className='notfound-wrapper'>
-            <div className='notfound-img-wrapper'>
-                <img src='/images/404.png' alt='404 logó' className='notfound-logo'/>
-                <img src='/images/404worker.png' alt='404 ember logó' className='notfound-worker' />
-            </div>
+        <>
+            <Seo
+                title="Pátyod Klíma | Az oldal nem található"
+                description="A keresett oldal nem található a Pátyod Klíma weboldalán."
+                noindex={true}
+            />
 
-            <div className='notfound-text'>
-                <h2>A keresett oldal nem található.</h2>
-                <p>Sajnos az oldal, amit keres, törölve lett, megváltozott a címe, vagy ideiglenesen nem elérhető.</p>
-            </div>
+            <div className='notfound-wrapper'>
+                <div className='notfound-img-wrapper'>
+                    <img src='/images/404.png' alt='404 logó' className='notfound-logo'/>
+                    <img src='/images/404worker.png' alt='404 ember logó' className='notfound-worker' />
+                </div>
 
-            <Link to="/" className='back-to-website-btn' target='_blank'>
-                Vissza a főoldalra
-            </Link>
-        </div>
+                <div className='notfound-text'>
+                    <h2>A keresett oldal nem található.</h2>
+                    <p>Sajnos az oldal, amit keres, törölve lett, megváltozott a címe, vagy ideiglenesen nem elérhető.</p>
+                </div>
+
+                <Link to="/" className='back-to-website-btn' target='_blank'>
+                    Vissza a főoldalra
+                </Link>
+            </div>
+        </>
     );
 }
